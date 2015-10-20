c7a147b9e117d6f5c29e4e66de3cb4556498956a
amoega1 <chocolitjunky@gmail.com>
2015-10-20 12:38:40 +0200

    Update BitCount.c

--- swes/BitCount.c
+++ swes/BitCount.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX 4294967295
+#define MAX 4294967295u
 #define MIN 0
 
 unsigned int count (unsigned int value) {
@@ -12,12 +12,16 @@ unsigned int count (unsigned int value) {
 }
 
 int main (int argc, char *argv[]) {
-    unsigned int _=0;
+    double _=0;
+    int n=sizeof(argv[1])/sizeof(char);
+
     if (argc!=2) { printf("error - enter a single integer argument\n"); return -1; }
-    if (sscanf (argv[1], "%u", &_)!=1) { printf ("error - not an integer\n"); return -1; }
-    else { 
-        if (_>MAX || _<MIN) {
-             printf("error - value should be 32 bit representable (0-4294967295)\n");
+    if (sscanf (argv[1], "%lf", &_)!=1) { printf ("error - not an integer\n"); return -1; }
+    else {
+        for (int i=0;i<n;++i){
+            if (argv[1][i] == '.' || argv[1][i] == '-') { printf("error - not a 32bit equivalent natural number\n"); return -1; }
+        }
+        if (_>MAX || _<MIN) { printf("error - value out of range\n");
              return -1;
         } else { printf("%d\n",count(_)); return 0; }
     }
