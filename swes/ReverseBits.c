9ff75c0d580c9adfbf595b31fee6d6463b8b2205
amoega1 <chocolitjunky@gmail.com>
2015-10-22 10:46:39 +0200

    Code style correction

--- swes/ReverseBits.c
+++ swes/ReverseBits.c
@@ -8,8 +8,9 @@ int main (int argc, char *argv[]) {
     unsigned int _;
     int n=sizeof(argv[1])/sizeof(char);
     if (argc!=2) { printf("error - enter a single integer argument\n"); return -1; }
-    if (sscanf (argv[1], "%i", &_)!=1) { printf("error - not an integer\n"); return -1; }
-    else {
+    if (sscanf (argv[1], "%i", &_)!=1) { 
+    	printf("error - not an integer\n"); return -1; 
+    } else {
         for (int i=0;i<n;++i){
             if (argv[1][i] == '.') { printf("error - not an integer\n"); return -1; }
         }
