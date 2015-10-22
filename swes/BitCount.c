b05022d943ea50e3b17a65bdeb06f096c16814e7
amoega1 <chocolitjunky@gmail.com>
2015-10-22 10:43:35 +0200

    Update BitCount.c

--- swes/BitCount.c
+++ swes/BitCount.c
@@ -16,13 +16,15 @@ int main (int argc, char *argv[]) {
     int n=sizeof(argv[1])/sizeof(char);
 
     if (argc!=2) { printf("error - enter a single integer argument\n"); return -1; }
-    if (sscanf (argv[1], "%lf", &_)!=1) { printf ("error - not an integer\n"); return -1; }
-    else {
+    if (sscanf (argv[1], "%lf", &_)!=1) {
+        printf ("error - not an integer\n"); return -1; 
+    } else {
         for (int i=0;i<n;++i){
             if (argv[1][i] == '.' || argv[1][i] == '-') { printf("error - not a 32bit equivalent natural number\n"); return -1; }
         }
-        if (_>MAX || _<MIN) { printf("error - value out of range\n");
-             return -1;
+        if (_>MAX || _<MIN) { 
+            printf("error - value out of range\n");
+            return -1;
         } else { printf("%d\n",count(_)); return 0; }
     }
 }
